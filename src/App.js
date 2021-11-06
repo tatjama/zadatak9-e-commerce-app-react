d523af5ae2d112c186ec20df1b7235d7664c1c0d
Tanja <tanja120a@gmail.com>
2021-11-07 00:18:19 +0100

    handle addProduct finished

--- src/App.js
+++ src/App.js
@@ -2,39 +2,26 @@ import React, { useState, useEffect } from 'react';
 
 import { Products, Navbar} from './components';
 
+const initialCart = [
+  {
+  id: 5,
+  title: "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+  price: 695,
+  category: "jewelery",
+  image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+  quantity: 1,
+},
+{
+  id: 9,
+  title: "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+  price: 64,
+  category: "electronics",
+  image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+  quantity: 3
+}
+]
 const App = () => {
-  /*const initialCart = {
-    id: 1,
-    products:[
-      {
-        productId: 7,
-        quantity: 1
-      },
-      {
-        productId: 8,
-        quantity: 2
-      }
-    ],
-    __v: 0
-  }*/
-  const initialCart = [
-        {
-        id: 5,
-        title: "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
-        price: 695,
-        category: "jewelery",
-        image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
-        quantity: 1,
-      },
-      {
-        id: 9,
-        title: "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
-        price: 64,
-        category: "electronics",
-        image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
-        quantity: 3
-      }
-    ]
+  
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState(initialCart);
 
@@ -52,29 +39,22 @@ const App = () => {
 
   const handleAddToCart = (productId, quantity) => {
     const itemFoundIndex = cart.findIndex(cp => cp.id === productId);
-    /**naci da li postoji id */
-    console.log(itemFoundIndex);
+
     let newCart;
     if(itemFoundIndex !== -1) {
-      cart[itemFoundIndex].quantity += quantity;
+      newCart = cart.map((item, i) => 
+        i === itemFoundIndex? { ...item, quantity: item.quantity + 1 } : item
+      )
+
     }else{
       const item = products.filter(p => p.id === productId);
       item[0].quantity = quantity;
-       newCart = [...cart, item];
-    }         
-    //let newCart = JSON.parse(JSON.stringify(cart));
-    //newCart.push(item);
-    //cart.products.push({productId: productId, quantity: quantity});
+       newCart = [...cart, ...item];
+    }
+    
     localStorage.setItem("cart", JSON.stringify(newCart));
     setCart(newCart);
   }
-
-  /*const handleAddToCart = (productId, quantity) => {
-    const item 
-    = cart.products.push({productId: productId, quantity: quantity});
-    localStorage.setItem("cart", JSON.stringify(item));
-    setCart(item);
-  }*/
 
   useEffect(() => {
     fetchProducts();
